64d26cb91233abcb3df128bfec6651a0a572e0fc
sunil <sunil.pundpal@hotmail.com>
2015-02-07 12:29:12 +0530

    Adpter Interface added

---
+++ DesignPattern/Adapter/AdapterToExcel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Adapter
+{
+    class AdapterToExcel : IAdapter
+    {
+
+        public void Do()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
